eee7b68c3ebe046890aa1566792e8f3110e08b7a
Xzcyy <109223715+Xzcyy@users.noreply.github.com>
2022-07-16 11:11:12 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
+*ɴᴀᴍᴇ:* Aditタ
+*ᴀɢᴇ:* 16ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 11 oct 2006
+*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Tengah, Purwokerto 
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
